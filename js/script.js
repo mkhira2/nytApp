8ec50666566575fa925d97b1cf8783a3c09f5e16
Kenji Hirabayashi <mkhira2@gmail.com>
2017-03-02 10:40:44 -0600

    backbone updated

--- js/script.js
+++ js/script.js
@@ -3,14 +3,10 @@
 //--------------------------------------------------
 
 var setHomePage = function() {
-	var containerNode = document.querySelector('.container')
+	var containerNode = document.querySelector('.content')
 	var html = ''
 		html += '<p>Welcome to our...website</p>'
 		containerNode.innerHTML = html
-}
-
-var setSearchPage = function() {
-	
 }
 
 var searchNode = document.querySelector('.search')
@@ -19,7 +15,21 @@ searchNode.addEventListener('keydown', function(eventObj) {
 	if (eventObj.keyCode === 13) {
 		var input = eventObj.target.value
 		location.hash = "search/" + input
+		eventObj.target.value = ''
 	}
+})
+
+//--------------------------------------------------
+//COLLECTION of MODELS
+//--------------------------------------------------
+
+var TimesCollection = Backbone.Collection.extend({
+	url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+	// Parse takes in the API response and will return the array that we want
+	parse: function(apiResponse) {
+		return apiResponse.response.docs
+	}
+
 })
 
 //--------------------------------------------------
@@ -38,7 +48,24 @@ var TimesRouter = Backbone.Router.extend({
 	},
 
 	showSearchPage: function(query) {
-		console.log('heres your' + query)
+		var collectionInstance = new TimesCollection()
+		console.log(collectionInstance)
+		var promise = collectionInstance.fetch({
+			data: {
+				q: query,
+				'api-key': 'fa162f9ec488494abf21f3f3b2225849'
+			}
+		})
+		promise.then(function() {
+			console.log(collectionInstance)
+			var htmlStr = ''
+			var docsArray = collectionInstance.models
+			for (var i = 0; i < docsArray.length; i++) {
+				var docModel = docsArray[i]
+				htmlStr += '<h3>' + docModel.get('snippet') + '</h3>'
+			}
+			document.querySelector('.content').innerHTML = htmlStr
+		})
 	}
 })
 
